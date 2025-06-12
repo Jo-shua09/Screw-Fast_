4df1c3c1a6724f60cec5e94ed222b20a63a1a765
jo-shua09 <joshuaonyeka039@gmail.com>
2025-06-12 23:49:33 +0100

    Fix className condition for company logos in Features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -38,7 +38,9 @@ export default function Features() {
 								alt={altText}
 								width={250}
 								height={150}
-								className={`w-[20rem] h-[7rem] object-contain ${!isDark ? "bg-neutral-600/50 !px-5 !py-1" : ""}`}
+								className={`w-[20rem] h-[7rem] object-contain ${
+									!isDark ? "bg-neutral-600/50 !px-5 !py-1" : "bg-neutral-600/50 !px-5 !py-1"
+								}`}
 							/>
 						);
 					})}
