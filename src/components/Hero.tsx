e10f02365ed432210b797d5ed98f771c4bd9e737
jo-shua09 <joshuaonyeka039@gmail.com>
2025-06-12 23:30:06 +0100

    Update button styles for improved responsiveness and consistency

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,23 +13,23 @@ import GlareHover from "./animations/GlareHover";
 
 export default function Hero() {
 	return (
-		<div className="w-full h-full  flex items-center justify-between flex-wrap md:flex-nowrap gap-10">
-			<div className="w-full md:flex-1">
-				<h1 className="text-8xl leading-30 font-semibold">
+		<div className="w-full h-full flex !flex-wrap lg:!mt-0 !mt-20 lg:!flex-nowrap items-center justify-between gap-20">
+			<div className="w-full lg:flex-1 text-center lg:text-left">
+				<h1 className="text-8xl lg:leading-30 font-semibold">
 					Equip Your Projects with <span className="text-yellow-500">ScrewFast</span>
 				</h1>
 
-				<p className=" font-normal !normal-case text-[1.7rem] !py-10">
+				<p className=" font-normal !normal-case text-[1.7rem] lg:!py-10 !py-7">
 					Top-quality hardware tools and expert construction services for every project need.
 				</p>
 
-				<div className="flex items-center gap-5 sm:flex-nowrap flex-wrap">
+				<div className="flex items-center gap-x-5 gap-y-10 justify-center lg:justify-start lg:flex-nowrap flex-wrap">
 					<ButtonOne wFull={true} buttonText="start exploring" />
 					<Buttontwo wFull={true} buttonText="contact sales team" />
 				</div>
 
-				<div className="w-full flex items-center gap-5 !mt-16">
-					<div className="flex items-center border-r-[.1rem] border-neutral-600 !pr-16">
+				<div className="w-full flex items-center gap-5 !mt-16 lg:justify-start justify-center">
+					<div className="flex items-center border-r-[.1rem] border-neutral-600 !pr-16 relative">
 						<Image src={personOne} alt="Person One" width={30} height={30} className="rounded-full" />
 						{[personTwo, personThree, personFour].map((person, index) => (
 							<Image
@@ -41,7 +41,7 @@ export default function Hero() {
 								className="rounded-full !-ml-5"
 							/>
 						))}
-						<div className="bg-black rounded-full text-white font-semibold text-2xl !-ml-5 w-[30px] h-[30px] flex items-center justify-center">
+						<div className="bg-black rounded-full text-white font-semibold text-2xl absolute right-3 sm:right-4 md:right-6 lg:right-8 w-[30px] h-[30px] flex items-center justify-center">
 							7k+
 						</div>
 					</div>
@@ -58,14 +58,14 @@ export default function Hero() {
 							</div>
 							<span className="text-2xl font-semibold">4.8/5</span>
 						</div>
-						<p className="text-2xl font-medium">
+						<p className="text-2xl font-medium text-nowrap">
 							From Over <span className="font-semibold">12.8k</span> Reviews
 						</p>
 					</div>
 				</div>
 			</div>
 
-			<div className="w-full md:flex-1 place-content-end flex">
+			<div className="w-full lg:flex-1 lg:place-content-end place-content-center flex">
 				<GlareHover
 					glareColor="#ffffff"
 					glareOpacity={0.3}
