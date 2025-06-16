b7d58df15ff2c4f59b0ddd1349acd1c9235b34e3
jo-shua09 <joshuaonyeka039@gmail.com>
2025-06-16 23:20:48 +0100

    Add text-center class to Button components for consistent alignment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export const ButtonOne = ({ buttonText, wFull }: ButtonProp) => {
 		<button
 			className={`${
 				wFull === true ? "lg:w-fit w-full" : "w-fit"
-			} flex gap-6 justify-between items-center lg:!py-5 !py-7 text-nowrap !px-6 cursor-pointer hover:scale-95 text-white  transition-all duration-300 shadow-2xl rounded-xl bg-orange-500 font-semibold text-[1.7rem]`}
+			} flex gap-6 justify-between items-center lg:!py-5 !py-7 text-center text-nowrap !px-6 cursor-pointer hover:scale-95 text-white  transition-all duration-300 shadow-2xl rounded-xl bg-orange-500 font-semibold text-[1.7rem]`}
 		>
 			{buttonText}
 			<MdOutlineKeyboardArrowRight className="text-3xl" />
@@ -20,7 +20,7 @@ export const Buttontwo = ({ buttonText, wFull }: ButtonProp) => {
 		<button
 			className={`${
 				wFull === true ? "lg:w-fit w-full" : "w-fit"
-			} flex gap-4 items-center lg:!py-5 !py-7 text-nowrap !px-6 cursor-pointer hover:scale-95 text-white transition-all duration-300 shadow-2xl rounded-xl bg-neutral-600  font-semibold text-[1.7rem]`}
+			} flex gap-4 items-center lg:!py-5 !py-7 text-center text-nowrap !px-6 cursor-pointer hover:scale-95 text-white transition-all duration-300 shadow-2xl rounded-xl bg-neutral-600  font-semibold text-[1.7rem]`}
 		>
 			{buttonText}
 		</button>
